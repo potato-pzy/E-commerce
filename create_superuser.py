927670ef26db2eb06ab136898ceffe5e4e3dde51
Ahzaaf.S <151323574+potato-pzy@users.noreply.github.com>
2025-06-23 04:20:09 +0530

    Update create_superuser.py

--- create_superuser.py
+++ create_superuser.py
@@ -8,8 +8,8 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'admin')
+if not User.objects.filter(username='admin1').exists():
+    User.objects.create_superuser('admin1', 'admin@example.com', 'admin')
     print("Superuser created: admin/admin")
 else:
     print("Superuser already exists.")
